f2a2be43911ec240a19125e65c619d640a67a0d6
MateuszGalinski <mateusz.galinski020@gmail.com>
2025-03-25 11:12:36 +0100

    Added cos policies for backend and tested http request for frontend

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -3,3 +3,25 @@
     Hello
   </div>
 </template>
+
+<script setup lang="ts"> 
+  import { ref, onMounted } from 'vue'
+  
+  const data = ref(null);
+  const error = ref(null);
+  
+  onMounted(async() => {
+    try {
+      const response = await fetch('http://localhost:8000/research/')
+      
+      if (!response.ok) throw new Error('Network response was not ok')
+        const result = await response.json()
+        data.value = result
+    } catch (err) {
+      error.value = err.message
+    } finally{
+      console.log(data)
+    }
+  
+  })
+</script>
