050b6c99e6874ce9fe712c0ef2f155b682c73475
jane <janeluciaschoenfeld@gmail.com>
2025-03-27 13:57:59 +0100

    ADD navbar

---
+++ Frontend/src/components/NavBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <nav class="bg-primary w-full flex justify-center rounded-tl-lg rounded-tr-lg fixed bottom-0 h-12">
+    <ul class="menu menu-horizontal">
+      <li>
+        <router-link to="/">Home</router-link>
+      </li>
+      <li>
+        <router-link to="/search">Search</router-link>
+      </li>
+      <li>
+        <router-link to="/chat">Chat</router-link>
+      </li>
+      <li>
+        <router-link to="/events">Events</router-link>
+      </li>
+      <li>
+        <router-link to="/bookmarks">Bookmarks</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
