01b57a0bdd0fcc4a16edd0dd4a2b42872c738655
jane <janeluciaschoenfeld@gmail.com>
2025-03-31 16:59:30 +0200

    ADD Card to pages

--- Frontend/src/views/Research.vue
+++ Frontend/src/views/Research.vue
@@ -1,1 +1,15 @@
-<template><h1>Research</h1></template>
+<template>
+  <div class="flex flex-col items-center justify-center gap-7">
+    <h1 class="">Research</h1>
+    <Card
+        card-image="https://http.cat/status/200.jpg"
+        card-image-alt="Cats"
+        card-title="Cats!"
+        card-text="Cats are great!"
+        button-title="Cuddle now"
+    />
+  </div>
+</template>
+<script setup lang="ts">
+import Card from "../components/Card.vue";
+</script>
--- Frontend/src/views/Researchers.vue
+++ Frontend/src/views/Researchers.vue
@@ -1,1 +1,16 @@
-<template><h1>Researchers</h1></template>
+<template>
+  <div class="flex flex-col items-center justify-center gap-7">
+    <h1 class="">Researchers</h1>
+    <Card
+        card-image="https://http.cat/status/200.jpg"
+        card-image-alt="Cats"
+        card-title="Cats!"
+        card-text="Cats are great!"
+        button-title="Cuddle now"
+        vertical
+    />
+  </div>
+</template>
+<script setup lang="ts">
+import Card from "../components/Card.vue";
+</script>
