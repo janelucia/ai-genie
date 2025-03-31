040eb1fb2608831c4de092fa80ad06fc4b1291d2
jane <janeluciaschoenfeld@gmail.com>
2025-03-31 17:05:52 +0200

    ADD collapseSection

---
+++ Frontend/src/components/collapseSection.vue
@@ -0,0 +1,14 @@
+<template>
+  <details class="collapse collapse-plus bg-base-100">
+    <summary class="collapse-title font-semibold">{{collapseTitle}}</summary>
+    <div class="collapse-content text-sm">
+      {{collapseText}}
+    </div>
+  </details>
+</template>
+<script setup lang="ts">
+defineProps<{
+  collapseTitle: string
+  collapseText: string
+}>()
+</script>
