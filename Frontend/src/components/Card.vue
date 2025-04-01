85b6f0aedf2b2ee41bb0ce0121791a364769a247
jane <janeluciaschoenfeld@gmail.com>
2025-04-01 10:44:27 +0200

    ADD custom spacing

--- Frontend/src/components/Card.vue
+++ Frontend/src/components/Card.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="card bg-base-100 w-full shadow-sm gap-4" :class="{'card-side': vertical}">
+  <div class="card bg-base-100 w-full gap-[var(--spacing-in-sections)]" :class="{'card-side': vertical}">
     <figure :class="{'w-1/3': vertical, 'h-28': !vertical}">
       <img
           :src="cardImage"
           :alt="cardImageAlt" />
     </figure>
-    <div class="card-body p-0">
+    <div class="card-body p-0 gap-[var(--spacing-in-sections)]" :class="{'pr-[var(--spacing-in-sections)]': vertical}">
       <Heading heading="h2" class="card-title pl-4">{{cardTitle}}</Heading>
       <Text class="pl-4">{{cardText}}</Text>
-      <div class="card-actions pt-4" :class="{'pl-4': vertical}">
-        <button class="btn btn-primary w-full">{{buttonTitle}}</button>
+      <div class="card-actions" :class="{'pl-4': vertical}">
+        <button class="btn btn-primary w-full">
+          <Text button>{{buttonTitle}}</Text>
+        </button>
       </div>
     </div>
   </div>
