562e18ae922aeaeb561ba5b9fd0110f4c010d9f6
jane <janeluciaschoenfeld@gmail.com>
2025-04-01 11:13:52 +0200

    ADD Floating Chat Button

---
+++ Frontend/src/components/FloatChatButton.vue
@@ -0,0 +1,8 @@
+<template>
+  <button class="btn btn-circle p-8 absolute bottom-16 right-4 bg-primary hover:bg-primary-focus">
+   <Text button>Chat</Text>
+  </button>
+</template>
+<script setup lang="ts">
+import Text from "./Text.vue";
+</script>
