ce3ba48df37490fbe3bb693ac7cc1be112ae15ee
jane <janeluciaschoenfeld@gmail.com>
2025-04-02 14:49:46 +0200

    ADD keywords

---
+++ Frontend/src/components/Keywords.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex gap-[var(--spacing-in-sections)]">
+    <div v-for="keyword in keywords" :key="keyword" class="badge badge-outline text-base-content badge-primary">{{keyword}}</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  defineProps<{
+    keywords: string[]
+  }>()
+</script>
