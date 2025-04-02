ea8e2a24bb470991eaa5a333d6498ba62b47a03b
jane <janeluciaschoenfeld@gmail.com>
2025-04-02 14:49:38 +0200

    FIX collapse section

--- Frontend/src/components/CollapseSection.vue
+++ Frontend/src/components/CollapseSection.vue
@@ -1,18 +1,15 @@
 <template>
   <details class="collapse collapse-plus">
-    <summary class="collapse-title font-semibold">
+    <summary class="collapse-title pl-0">
       <Heading heading="h3">{{collapseTitle}}</Heading>
     </summary>
     <div class="collapse-content w-full">
-      <Text class="break-words break-all whitespace-pre-wrap">
-        <slot />
-      </Text>
+      <slot />
     </div>
   </details>
 </template>
 <script setup lang="ts">
 import Heading from "./Heading.vue";
-import Text from "./Text.vue";
 
 defineProps<{
   collapseTitle: string
