305f797516d3fda0394fe24bae908fd7b8401b87
jane <janeluciaschoenfeld@gmail.com>
2025-04-07 10:50:23 +0200

    ADD sign up form

--- Frontend/src/components/Header.vue
+++ Frontend/src/components/Header.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="flex w-full justify-between">
+  <div class="flex w-full justify-between gap-[var(--spacing-in-sections)] items-center fixed bg-base-100 top-0 left-0 z-10 p-4">
     <button class="btn btn-secondary" @click="router.go(-1)">
       <Text>Back</Text>
     </button>
-    <Heading heading="h1" class="flex-grow text-center">
-      {{headerTitle}}
-    </Heading>
+    <Text class="flex-grow text-center font-bold">
+      Oslo Met AI Lab
+    </Text>
     <div class="w-16"></div>
   </div>
 </template>
 <script setup lang="ts">
 import router from "../router";
 import Text from "./Text.vue";
-import Heading from "./Heading.vue";
 
 defineProps<{
   headerTitle: string
