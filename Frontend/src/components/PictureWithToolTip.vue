98680cb811c0dec5db4aa1a14e9ee3192f7cfc5a
jane <janeluciaschoenfeld@gmail.com>
2025-04-07 12:57:16 +0200

    ADD tooltip component for pictures

---
+++ Frontend/src/components/PictureWithToolTip.vue
@@ -0,0 +1,32 @@
+<template>
+  <figure>
+    <div class="relative w-full h-full rounded-lg">
+      <img
+        :src="image"
+        :alt="imageAlt"
+        class="w-full h-full object-cover"
+        :class="pictureClass"
+      />
+
+      <div
+        v-if="tooltipText"
+        class="absolute bottom-2 right-2 tooltip tooltip-info tooltip-left"
+        :data-tip="tooltipText"
+      >
+        <button class="btn btn-xs btn-circle btn-info">
+          <Text> i </Text>
+        </button>
+      </div>
+    </div>
+  </figure>
+</template>
+<script setup lang="ts">
+import Text from "./Text.vue";
+
+defineProps<{
+  image: string;
+  imageAlt: string;
+  tooltipText?: string;
+  pictureClass?: string;
+}>();
+</script>
