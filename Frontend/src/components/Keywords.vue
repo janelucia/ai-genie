713a6d4c5adb9a09681185e2f1ccfceb6a83ff79
jane <janeluciaschoenfeld@gmail.com>
2025-04-30 16:05:00 +0200

    feat: secondary text color for keywords

--- Frontend/src/components/Keywords.vue
+++ Frontend/src/components/Keywords.vue
@@ -3,7 +3,7 @@
     <div
       v-for="keyword in keywords"
       :key="keyword"
-      class="badge badge-outline text-base-content badge-secondary"
+      class="badge badge-outline badge-secondary"
       @click="router.push('/research?keywords=' + keyword)"
     >
       {{ keyword }}
