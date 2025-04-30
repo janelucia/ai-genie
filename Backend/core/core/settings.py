44eeced33daa97f66255ebb08eb865af90a52a07
MateuszGalinski <mateusz.galinski020@gmail.com>
2025-04-30 23:45:58 +0200

    Added theming option to admin panel

--- Backend/core/core/settings.py
+++ Backend/core/core/settings.py
@@ -37,6 +37,8 @@ CORS_ALLOWED_ORIGINS = [
 # Application definition
 
 INSTALLED_APPS = [
+    "admin_interface",
+    "colorfield",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
