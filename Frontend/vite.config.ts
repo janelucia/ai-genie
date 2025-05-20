83167d4abc15eecf68f6b2336a87107a4b991206
jane <janeluciaschoenfeld@gmail.com>
2025-05-20 12:56:53 +0200

    feat: add vitest

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
@@ -7,10 +8,10 @@ export default defineConfig({
   plugins: [vue(), tailwindcss()],
   server: {
     port: 3000,
-    host: true,  // <--- Allow all hosts (necessary for ngrok to tunnel into dev server)
+    host: true, // <--- Allow all hosts (necessary for ngrok to tunnel into dev server)
     strictPort: true, // Don't switch port if 3000 is busy (optional but nice for consistency)
     allowedHosts: [
-      "393e-158-36-93-250.ngrok-free.app",  // Allow ngrok frontend URL
+      "393e-158-36-93-250.ngrok-free.app", // Allow ngrok frontend URL
     ],
   },
 });
