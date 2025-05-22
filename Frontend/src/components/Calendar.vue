709b41cdef8049c0ff1544233e945080d2049077
jane <janeluciaschoenfeld@gmail.com>
2025-05-22 12:39:27 +0200

    fix: calendar

--- Frontend/src/components/Calendar.vue
+++ Frontend/src/components/Calendar.vue
@@ -4,27 +4,24 @@
     ref="calendarRef"
     @change="onDateChange"
   >
-    <template v-slot:previous>
-      <svg
-        aria-label="Previous"
-        class="fill-current size-4"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-      >
-        <path fill="currentColor" d="M15.75 19.5 8.25 12l7.5-7.5" />
-      </svg>
-    </template>
-
-    <template v-slot:next>
-      <svg
-        aria-label="Next"
-        class="fill-current size-4"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-      >
-        <path fill="currentColor" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
-      </svg>
-    </template>
+    <svg
+      aria-label="Previous"
+      class="fill-current size-4"
+      slot="previous"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+    >
+      <path fill="currentColor" d="M15.75 19.5 8.25 12l7.5-7.5"></path>
+    </svg>
+    <svg
+      aria-label="Next"
+      class="fill-current size-4"
+      slot="next"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+    >
+      <path fill="currentColor" d="m8.25 4.5 7.5 7.5-7.5 7.5"></path>
+    </svg>
 
     <calendar-month></calendar-month>
   </calendar-date>
