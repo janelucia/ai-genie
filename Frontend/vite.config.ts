6237d2dabfc0c3437f46cb193c12e89c680adfea
jane <janeluciaschoenfeld@gmail.com>
2025-05-22 12:40:11 +0200

    fix: calendar

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -5,7 +5,16 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [
+    vue({
+      template: {
+        compilerOptions: {
+          isCustomElement: (tag) => tag.includes("calendar-"),
+        },
+      },
+    }),
+    tailwindcss(),
+  ],
   test: {
     environment: "jsdom",
   },
